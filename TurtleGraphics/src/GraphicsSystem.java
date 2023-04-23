2324001a48754610a3504503f5b2451171deefb4
Josh <Josh@DESKTOP-3IFDSBV.Home>
2023-04-23 23:24:32 +0100

    Reorganised to reduce clutter.

--- TurtleGraphics/src/GraphicsSystem.java
+++ TurtleGraphics/src/GraphicsSystem.java
@@ -20,10 +20,8 @@ public class GraphicsSystem extends LBUGraphics
 	
 	ArrayList<String> allUserInput = new ArrayList<>();
 
-	static final String[] PARAM_COMMANDS = new String[] {"saveimage", "loadimage", "savecommands", "loadcommands", "forward", "backward", "turnleft", "turnright",
-			"square", "pencolour", "penwidth", "triangle"};
-
-	static final String[] NO_PARAM_COMMANDS = new String[] {"about", "penup", "pendown", "black", "green", "red", "white", "reset", "clear"};
+	private final String[] COMMANDS = new String[] {"about", "penup", "pendown", "black", "green", "red", "white", "reset", "clear", "saveimage", "loadimage",
+			"savecommands", "loadcommands", "forward", "backward", "turnleft", "turnright", "square", "penwidth", "triangle", "pencolour"};
 
 	boolean savedCmd = true, savedImg = true;
 	
@@ -55,27 +53,42 @@ public class GraphicsSystem extends LBUGraphics
 		String[] userInput = command.toLowerCase().split(" ");
 		String cmd = userInput[0];
 
-		List<String> paramArray = new ArrayList<>(Arrays.asList(PARAM_COMMANDS));
-		List<String> noParamArray = new ArrayList<>(Arrays.asList(NO_PARAM_COMMANDS));
+		List<String> commandsArrayList = new ArrayList<>(Arrays.asList(COMMANDS));
+		Runnable[] methodArray = new Runnable[COMMANDS.length];
 
-		if(paramArray.contains(cmd) && userInput.length > 1 || noParamArray.contains(cmd))
+		if(commandsArrayList.contains(cmd))
 		{
-			if(userInput.length > 1)
+			if(setMethodArray(allUserInput, methodArray))
 			{
-				cmdParam(PARAM_COMMANDS, allUserInput);
-				posCheck(turtlePos);	
-			} 
-			
-			else if (userInput.length == 1)
-			{
-				cmdNoParam(cmd, NO_PARAM_COMMANDS, allUserInput);
-				posCheck(turtlePos);
+				try
+				{
+					for(int i = 0; i < COMMANDS.length; i++)
+					{
+						if(cmd.equals(COMMANDS[i]))
+						{
+							methodArray[i].run();
+							displayMessage("LBUgraphics V4.4");
+							if(cmd.equals("clear") || cmd.substring(0, 4).equals("save") || cmd.substring(0, 4).equals("load"))
+							{
+								savedCmd = true;
+								savedImg = true;
+							} 
+							
+							else
+							{
+								savedCmd = false;
+								savedImg = false;
+							}
+						}else continue;
+					}
+					posCheck(turtlePos);	
+				}
+				
+				catch(java.lang.NullPointerException e)
+				{
+					displayMessage("Command requires a parameter.");
+				}
 			}
-		} 
-		
-		else if(paramArray.contains(cmd))
-		{
-			displayMessage("Command requires a parameter.");
 		} 
 		
 		else
@@ -84,142 +97,125 @@ public class GraphicsSystem extends LBUGraphics
 		}
 	}
 
-	private void cmdParam(String[] ParamCommands, ArrayList<String> allUserInput)
+	private boolean setMethodArray(ArrayList<String> allUserInput, Runnable[] methodArray)
 	{
-		String lastUserInput = allUserInput.get(allUserInput.size() - 1);
-		String[] userInput = lastUserInput.toLowerCase().split(" ");
-		String cmd = userInput[0];
-
-		Runnable[] paramArray = new Runnable[PARAM_COMMANDS.length];
-		paramArray[0] = () -> handleImg("save", getBufferedImage(), userInput[1]);
-		paramArray[1] = () -> handleImg("load", getBufferedImage(), userInput[1]);
-		paramArray[2] = () -> handleCmd("save", allUserInput, userInput[1]);
-		paramArray[3] = () -> handleCmd("load", allUserInput, userInput[1]);
-
-		try
+	String[] userInput = allUserInput.get(allUserInput.size() - 1).toLowerCase().split(" ");
+	String cmd = userInput[0];
+	
+	String[] oneParam = {"saveimage", "loadimage", "savecommands", "loadcommands", "forward", "backward", "turnleft", "turnright", "square", "penwidth"};
+	String[] threeParam = {"triangle", "pencolour"};
+	
+		if(userInput.length == 1)
 		{
-			if(cmd.equals("triangle"))
+			methodArray[0] = () -> about();
+			methodArray[1] = () -> penUp();
+			methodArray[2] = () -> penDown();
+			methodArray[3] = () -> setPenColour(Color.black);
+			methodArray[4] = () -> setPenColour(Color.green);
+			methodArray[5] = () -> setPenColour(Color.red);
+			methodArray[6] = () -> setPenColour(Color.white);
+			methodArray[7] = () ->
 			{
-				triangle(Integer.parseInt(userInput[1]), Integer.parseInt(userInput[2]), Integer.parseInt(userInput[3]));
-			}
-
-			else
-			{
-				int R = Integer.parseInt(userInput[1]);
-				int G = Integer.parseInt(userInput[2]);
-				int B = Integer.parseInt(userInput[3]);
-
-				if((0 <= R && R <= 255) && (0 <= G && G <= 255) && (0 <= B && B <= 255))
-				{
-					penColour(R, G, B);
-				} else displayMessage("RGB values must be between 0 and 255.");
-			}
-		} 
-		
-		catch(ArrayIndexOutOfBoundsException e)
-		{
-			int numAmount = Integer.parseInt(userInput[1]);
-
-			paramArray[4] = () -> forward(numAmount);
-			paramArray[5] = () -> forward(-numAmount);
-			paramArray[6] = () -> turnLeft(numAmount);
-			paramArray[7] = () -> turnRight(numAmount);
-			paramArray[8] = () -> square(numAmount);
-			paramArray[9] = () ->{};
-			paramArray[10] = () -> penWidth(numAmount);
-			paramArray[11] = () -> triangle(numAmount);
-
-			if((cmd.equals("forward") || cmd.equals("backward")) && (0 >= numAmount || numAmount > 100))
-			{
-				displayMessage("Paramater must be an Integer between 1 and 100");
-				allUserInput.remove(allUserInput.size() - 1);
-			}
+				reset();
+				penDown();
+				setPenColour(Color.red);
+				penWidth(1);
+			};
+			methodArray[8] = () -> clear();
 			
-			else if((cmd.equals("turnright") || cmd.equals("turnleft")) && (0 >= numAmount || numAmount > 360))
-			{
-				displayMessage("Paramater must be an Integer between 1 and 360");
-				allUserInput.remove(allUserInput.size() - 1);
-			}
-			
-			else if(userInput.length > 2)
-			{
-				displayMessage("Triangle command requires 1 or 3 paramaters.");
-			}
-			
-			else
-			{
-				for(int i = 0; i < ParamCommands.length; i++)
-				{
-					if(cmd.equals(ParamCommands[i]))
-					{
-						paramArray[i].run();
-						savedCmd = false;
-						savedImg = false;
-						break;
-					} else continue;
-				}
-			}
-		} 
-		
-		catch(Exception e)
-		{
-			if(cmd.substring(0, 4).equals("save") || cmd.substring(0, 4).equals("load"))
-			{
-				for(int i = 0; i < 4; i++)
-				{
-					if(cmd.equals(ParamCommands[i]))
-					{
-						paramArray[i].run();
-						break;
-					} else continue;
-				}
-			} 
-			
-			else 
-			{
-				displayMessage("Parameter requires Integer.");
-				allUserInput.remove(allUserInput.size() - 1);
-			}
+			return true;
 		}
-	}
-
-	private void cmdNoParam(String cmd, String[] noParamCommands, ArrayList<String> allUserInput)
-	{
-		Runnable[] noParamArray = new Runnable[noParamCommands.length];
-		noParamArray[0] = () -> about();
-		noParamArray[1] = () -> penUp();
-		noParamArray[2] = () -> penDown();
-		noParamArray[3] = () -> setPenColour(Color.black);
-		noParamArray[4] = () -> setPenColour(Color.green);
-		noParamArray[5] = () -> setPenColour(Color.red);
-		noParamArray[6] = () -> setPenColour(Color.white);
-		noParamArray[7] = () ->
+		
+		else if(userInput.length == 2)
 		{
-			reset();
-			penDown();
-			setPenColour(Color.red);
-			penWidth(1);
-		};
-		noParamArray[8] = () -> clear();
-
-		for(int i = 0; i < noParamCommands.length; i++)
-		{
-			if(cmd.equals(noParamCommands[i]))
+			methodArray[9] = () -> handleImg("save", getBufferedImage(), userInput[1]);
+			methodArray[10] = () -> handleImg("load", getBufferedImage(), userInput[1]);
+			methodArray[11] = () -> handleCmd("save", allUserInput, userInput[1]);
+			methodArray[12] = () -> handleCmd("load", allUserInput, userInput[1]);
+			
+			try
 			{
-				noParamArray[i].run();
-				if(!cmd.equals("clear"))
+				if(!cmd.equals("savecommands") && !cmd.equals("loadcommands") && !cmd.equals("saveimage") && !cmd.equals("loadimage"))
 				{
-					savedCmd = false;
-					savedImg = false;
-				} 
+					int numAmount = Integer.parseInt(userInput[1]);
+					
+					methodArray[13] = () -> forward(numAmount);
+					methodArray[14] = () -> forward(-numAmount);
+					methodArray[15] = () -> turnLeft(numAmount);
+					methodArray[16] = () -> turnRight(numAmount);
+					methodArray[17] = () -> square(numAmount);
+					methodArray[18] = () -> penWidth(numAmount);
+					methodArray[19] = () -> triangle(numAmount);	
 				
-				else
-				{
-					savedCmd = true;
-					savedImg = true;
+					if(!Arrays.asList(oneParam).contains(cmd))
+					{
+						displayMessage("Command requires no parameters.");
+						return false;
+					}
+					
+					else if(userInput.length > 2 && cmd.equals("triangle"))
+					{
+						displayMessage("Triangle command requires 1 or 3 paramaters.");
+						return false;
+					}
+					
+					else if((cmd.equals("forward") || cmd.equals("backward")) && (0 >= numAmount || numAmount > 100))
+					{
+						displayMessage("Command must be an Integer between 1 and 100");
+						allUserInput.remove(allUserInput.size() - 1);
+						return false;
+					}
+					
+					else if((cmd.equals("turnright") || cmd.equals("turnleft")) && (0 >= numAmount || numAmount > 360))
+					{
+						displayMessage("Command must be an Integer between 1 and 360");
+						allUserInput.remove(allUserInput.size() - 1);
+						return false;
+					}else return true;
 				}
-			}else continue;
+			}
+			
+			catch(NumberFormatException e)
+			{
+				displayMessage("Command requires Integer.");
+				allUserInput.remove(allUserInput.size() - 1);	
+				return false;
+			}
 		}
+		
+		else if(userInput.length > 2)
+		{
+			try
+			{
+				if(!Arrays.asList(threeParam).contains(cmd))
+				{
+					displayMessage("Command does not require multiple parameters");
+					return false;
+				}
+				else if(Arrays.asList(threeParam).contains(cmd) && userInput.length > 4)
+				{
+					displayMessage("Command requires 3 parameters.");
+					return false;
+				}
+				
+				int num1 = Integer.parseInt(userInput[1]);
+				int num2 = Integer.parseInt(userInput[2]);
+				int num3 = Integer.parseInt(userInput[3]);
+				
+				methodArray[19] = () -> triangle(num1, num2, num3); 
+				methodArray[20] = () -> penColour(num1, num2, num3); 
+				
+				return true;
+			}
+			
+			catch(NumberFormatException e)
+			{
+				displayMessage("Command requires Integer.");
+				allUserInput.remove(allUserInput.size() - 1);	
+				return false;
+			}
+		}
+		return false;
 	}
 	
 	private void posCheck(int[] turtlePos)
